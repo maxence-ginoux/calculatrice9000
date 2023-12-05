635557de38114d9199b0e541327a8849f8300e2d
maxence.ginoux <maxence.ginoux@laplateforme.io>
2023-12-05 09:10:00 +0100

    modif

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-num1 = input("veuillez taper le premier nombre : ")
-operator = str(input("veuillez taper le type d'opération : "))
-num2 = int(input("veuillez taper le deuxième nombre : "))
+
 
 def calcule (num1, operator, num2):
     num1 = float(num1)
@@ -12,28 +10,27 @@ def calcule (num1, operator, num2):
     resultat_multiplication = num1 * num2
                                         
     if operator == "+":
-        if num1 == int(input("veuillez taper le premier nombre : ")):
-            print(f"est égale à {resultat_addition}")
-        else:
-            print("COUCOU SOSO")
+        return f"{num1} {operator} {num2} est égale à {resultat_addition}"
     elif operator == "-":
-        return f"est égale à {resultat_soustraction}"
+        return f"{num1} {operator} {num2} est égale à {resultat_soustraction}"
     elif operator == "/":
-        if num2 != 0:
-            return f"est égale à {resultat_division}"
-        else:
-            return "erreur ! division par 0 impossible !"
+        return f"{num1} {operator} {num2} est égale à {resultat_division}"
     elif operator == "*":
-        return f"est égale à {resultat_multiplication}"
+        return f"{num1} {operator} {num2} est égale à {resultat_multiplication}"
     else:
-        return "opération impossible !"
+        return "ERREUR opération impossible ! veuillez saisir un opérateur valide !"
     
-    
-resultat = calcule(num1, operator, num2)
-print(resultat)
 
+while True:
+    try:
+        num1 = input("veuillez taper le premier nombre : ")
+        operator = str(input("veuillez taper le type d'opération : "))
+        num2 = int(input("veuillez taper le deuxième nombre : "))
+        resultat = calcule(num1, operator, num2)
+        print(resultat)
+        break
 
-#print(calcule(9, "+", 10))
-#print(calcule(9, "-", 10))
-#print(calcule(9, "/", 10))
-#print(calcule(9, "*", 10))
+    except ZeroDivisionError:
+        print("La division par zéro n'est pas autorisée. Veuillez réessayer.")
+    except Exception:
+        print("Une erreur s'est produite. Veuillez réessayer.")
